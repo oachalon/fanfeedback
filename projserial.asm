179d85d3f840c5a52a43d3ed9e2d4895de41d251
oachalon <rjnuti@gmail.com>
2017-04-18 18:01:17 -0400

    Add files via upload

--- projserial.asm
+++ projserial.asm
@@ -179,7 +179,10 @@ Text
 ;====================================== MAIN PROGRAM ===================================
 Main
  	call Init ; Intialization
-
+;	banksel PIR1
+;	btfss PIR1, TMR1IF
+;	call Message
+;	goto SpeedLoop
 ;------------------------------- TACHOMETER INPUT INTO PORTA,1 -----------------------------------------
 SpeedLoop 
 	btfss PORTA,1
@@ -188,6 +191,7 @@ SpeedLoop
  	btfsc PORTA,1
  	goto $-1
  	btfss PIR1,TMR1IF
+
 goto SpeedLoop
 
 Increment
@@ -246,10 +250,10 @@ Init
 
 ; Timer 1 Setup
 
- 	movlw 0X09
+ 	movlw 0X00
  	banksel TMR1H
  	movwf TMR1H ;Timer= 0000-FFFFH = 65535*8/(8M/4) = 0.26214s Prescale = 8
- 	movlw 0XFF
+ 	movlw 0X00
  	banksel TMR1L
  	movwf TMR1L
 
@@ -282,6 +286,7 @@ SERIAL_SETUP:
  	banksel RCSTA
  	movwf RCSTA ;Enable serial port & continuous reception
 
+; Clear Ports and Counts
 	clrf	count
 	clrf	PORTA
 	clrf	PORTB
@@ -296,10 +301,10 @@ SetPorts
 	movwf	LCD_TRIS
 	movwf	TRISD  
 		
-;	banksel LCD_Init
+	banksel LCD_Init
 
-;	call	LCD_Init		;setup LCD
-;	clrf	count			;set counter register to zero
+	call	LCD_Init		;setup LCD
+	clrf	count			;set counter register to zero
 
 
 
@@ -377,199 +382,199 @@ pop
 ;LCD routines
 
 ;Initialise LCD
-;LCD_Init	call 	LCD_Busy		;wait for LCD to settle
+LCD_Init	call 	LCD_Busy		;wait for LCD to settle
 
-;		movlw	0x20			;Set 4 bit mode
-;		call	LCD_Cmd
+		movlw	0x20			;Set 4 bit mode
+		call	LCD_Cmd
 
-;		movlw	0x28			;Set display shift
-;		call	LCD_Cmd
+		movlw	0x28			;Set display shift
+		call	LCD_Cmd
 
-;		movlw	0x06			;Set display character mode
-;		call	LCD_Cmd
-;;
-;		movlw	0x0c			;Set display on/off and cursor command
-;		call	LCD_Cmd			;Set cursor off
-;
-;		call	LCD_Clr			;clear display
-;
-;		retlw	0x00
+		movlw	0x06			;Set display character mode
+		call	LCD_Cmd
+
+		movlw	0x0c			;Set display on/off and cursor command
+		call	LCD_Cmd			;Set cursor off
+
+		call	LCD_Clr			;clear display
+
+		retlw	0x00
 
 ; command set routine
-;LCD_Cmd		
-;		movwf	templcd
-;		swapf	templcd,	w	;send upper nibble
-;		andlw	0x0f			;clear upper 4 bits of W
-;		movwf	LCD_PORT
-;		bcf	LCD_PORT, LCD_RS	;RS line to 1
-;		call	Pulse_e			;Pulse the E line high
+LCD_Cmd		
+		movwf	templcd
+		swapf	templcd,	w	;send upper nibble
+		andlw	0x0f			;clear upper 4 bits of W
+		movwf	LCD_PORT
+		bcf	LCD_PORT, LCD_RS	;RS line to 1
+		call	Pulse_e			;Pulse the E line high
 
-;		movf	templcd,	w	;send lower nibble
-;		andlw	0x0f			;clear upper 4 bits of W
-;		movwf	LCD_PORT
-;		bcf	LCD_PORT, LCD_RS	;RS line to 1
-;		call	Pulse_e			;Pulse the E line high
-;		call 	LCD_Busy
-;		return
+		movf	templcd,	w	;send lower nibble
+		andlw	0x0f			;clear upper 4 bits of W
+		movwf	LCD_PORT
+		bcf	LCD_PORT, LCD_RS	;RS line to 1
+		call	Pulse_e			;Pulse the E line high
+		call 	LCD_Busy
+		return
 
 
-;LCD_CharD	
-;addlw	0x30			;add 0x30 to convert to ASCII
-;LCD_Char	
-;		movwf	templcd
-;		swapf	templcd,	w	;send upper nibble
-;		andlw	0x0f			;clear upper 4 bits of W
-;		movwf	LCD_PORT
-;		bsf	LCD_PORT, LCD_RS	;RS line to 1
-;		call	Pulse_e			;Pulse the E line high
+LCD_CharD	
+addlw	0x30			;add 0x30 to convert to ASCII
+LCD_Char	
+		movwf	templcd
+		swapf	templcd,	w	;send upper nibble
+		andlw	0x0f			;clear upper 4 bits of W
+		movwf	LCD_PORT
+		bsf	LCD_PORT, LCD_RS	;RS line to 1
+		call	Pulse_e			;Pulse the E line high
 
-;		movf	templcd,	w	;send lower nibble
-;		andlw	0x0f			;clear upper 4 bits of W
-;		movwf	LCD_PORT
-;		bsf	LCD_PORT, LCD_RS	;RS line to 1
-;		call	Pulse_e			;Pulse the E line high
-;		call 	LCD_Busy
-;		retlw	0x00
+		movf	templcd,	w	;send lower nibble
+		andlw	0x0f			;clear upper 4 bits of W
+		movwf	LCD_PORT
+		bsf	LCD_PORT, LCD_RS	;RS line to 1
+		call	Pulse_e			;Pulse the E line high
+		call 	LCD_Busy
+		retlw	0x00
 
-;LCD_Line1	
-;		movlw	0x80			;move to 1st row, first column
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_Line1	
+		movlw	0x80			;move to 1st row, first column
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_Line2	
-;		movlw	0xc0			;move to 2nd row, first column
-;		call	LCD_Cmd
-;		retlw	0x00
-;
-;LCD_Line1W	
-;		addlw	0x80			;move to 1st row, column W
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_Line2	
+		movlw	0xc0			;move to 2nd row, first column
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_Line2W	addlw	0xc0			;move to 2nd row, column W
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_Line1W	
+		addlw	0x80			;move to 1st row, column W
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_CurOn	movlw	0x0d			;Set display on/off and cursor command
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_Line2W	addlw	0xc0			;move to 2nd row, column W
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_CurOff	movlw	0x0c			;Set display on/off and cursor command
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_CurOn	movlw	0x0d			;Set display on/off and cursor command
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_Clr		movlw	0x01			;Clear display
-;		call	LCD_Cmd
-;		retlw	0x00
+LCD_CurOff	movlw	0x0c			;Set display on/off and cursor command
+		call	LCD_Cmd
+		retlw	0x00
 
-;LCD_HEX		movwf	tmp1
-;		swapf	tmp1,	w
-;		andlw	0x0f
-;		call	HEX_Table
-;		call	LCD_Char
-;		movf	tmp1, w
-;		andlw	0x0f
-;		call	HEX_Table
-;		call	LCD_Char
-;		retlw	0x00
+LCD_Clr		movlw	0x01			;Clear display
+		call	LCD_Cmd
+		retlw	0x00
 
-;Delay255	movlw	0xFF			;delay 255 mS
-;		goto	d4
-;Delay100	movlw	d'100'			;delay 100mS
-;		goto	d4
-;Delay50		movlw	d'50'			;delay 50mS
-;		goto	d4
-;Delay20		movlw	d'20'			;delay 20mS
-;		goto	d4
-;Delay5		movlw	0x05			;delay 5.000 ms (8 MHz clock)
-;		banksel	count1
-;d4		movwf	count1
-;d5		movlw	0x8F			;delay 1mS
-;		movwf	counta
-;		movlw	0x02
-;		movwf	countb
-;Delay_0
-;		decfsz	counta, f
-;		goto	$+2
-;		decfsz	countb, f
-;		goto	Delay_0
+LCD_HEX		movwf	tmp1
+		swapf	tmp1,	w
+		andlw	0x0f
+		call	HEX_Table
+		call	LCD_Char
+		movf	tmp1, w
+		andlw	0x0f
+		call	HEX_Table
+		call	LCD_Char
+		retlw	0x00
 
-;		decfsz	count1	,f
-;		goto	d5
-;		return
+Delay255	movlw	0xFF			;delay 255 mS
+		goto	d4
+Delay100	movlw	d'100'			;delay 100mS
+		goto	d4
+Delay50		movlw	d'50'			;delay 50mS
+		goto	d4
+Delay20		movlw	d'20'			;delay 20mS
+		goto	d4
+Delay5		movlw	0x05			;delay 5.000 ms (8 MHz clock)
+		banksel	count1
+d4		movwf	count1
+d5		movlw	0x8F			;delay 1mS
+		movwf	counta
+		movlw	0x02
+		movwf	countb
+Delay_0
+		decfsz	counta, f
+		goto	$+2
+		decfsz	countb, f
+		goto	Delay_0
 
-;Pulse_e		bsf	LCD_PORT, LCD_E
-;		nop
-;		bcf	LCD_PORT, LCD_E
-;		return
+		decfsz	count1	,f
+		goto	d5
+		return
 
-;LCD_Busy
-;		bsf	STATUS,	RP0		;set bank 1
-;		movlw	0x0f			;set Port for input
-;		movwf	LCD_TRIS
-;		bcf	STATUS,	RP0		;set bank 0
-;		bcf	LCD_PORT, LCD_RS	;set LCD for command mode
-;		bsf	LCD_PORT, LCD_RW	;setup to read busy flag
-;		bsf	LCD_PORT, LCD_E
-;		swapf	LCD_PORT, w		;read upper nibble (busy flag)
-;		bcf	LCD_PORT, LCD_E		
-;		movwf	templcd2 
-;		bsf	LCD_PORT, LCD_E		;dummy read of lower nibble
-;		bcf	LCD_PORT, LCD_E
-;		btfsc	templcd2, 7		;check busy flag, high = busy
-;		goto	LCD_Busy		;if busy check again
-;		bcf	LCD_PORT, LCD_RW
-;		bsf	STATUS,	RP0		;set bank 1
-;		movlw	0x00			;set Port for output
-;		movwf	LCD_TRIS
-;		bcf	STATUS,	RP0		;set bank 0
-;		return
+Pulse_e		bsf	LCD_PORT, LCD_E
+		nop
+		bcf	LCD_PORT, LCD_E
+		return
+
+LCD_Busy
+		bsf	STATUS,	RP0		;set bank 1
+		movlw	0x0f			;set Port for input
+		movwf	LCD_TRIS
+		bcf	STATUS,	RP0		;set bank 0
+		bcf	LCD_PORT, LCD_RS	;set LCD for command mode
+		bsf	LCD_PORT, LCD_RW	;setup to read busy flag
+		bsf	LCD_PORT, LCD_E
+		swapf	LCD_PORT, w		;read upper nibble (busy flag)
+		bcf	LCD_PORT, LCD_E		
+		movwf	templcd2 
+		bsf	LCD_PORT, LCD_E		;dummy read of lower nibble
+		bcf	LCD_PORT, LCD_E
+		btfsc	templcd2, 7		;check busy flag, high = busy
+		goto	LCD_Busy		;if busy check again
+		bcf	LCD_PORT, LCD_RW
+		bsf	STATUS,	RP0		;set bank 1
+		movlw	0x00			;set Port for output
+		movwf	LCD_TRIS
+		bcf	STATUS,	RP0		;set bank 0
+		return
 
 ;end of LCD routines
 
 
 
 ;This routine downloaded from http://www.piclist.com
-;Convert:                        ; Takes number in NumH:NumL
+Convert:                        ; Takes number in NumH:NumL
                                 ; Returns decimal in
                                 ; TenK:Thou:Hund:Tens:Ones
-;
-;        swapf   NumH, w
-;	iorlw	B'11110000'
-;       movwf   Thou
-;       addwf   Thou,f
-;        addlw   0XE2
-;        movwf   Hund
- ;       addlw   0X32
- ;       movwf   Ones
-;
-;        movf    NumH,w
-;        andlw   0X0F
-;        addwf   Hund,f
-;        addwf   Hund,f
- ;       addwf   Ones,f
-;        addlw   0XE9
-  ;      movwf   Tens
-;        addwf   Tens,f
- ;       addwf   Tens,f
 
-;        swapf   NumL,w
-;        andlw   0X0F
-;        addwf   Tens,f
-;        addwf   Ones,f
+        swapf   NumH, w
+	iorlw	B'11110000'
+       movwf   Thou
+       addwf   Thou,f
+        addlw   0XE2
+        movwf   Hund
+       addlw   0X32
+        movwf   Ones
 
-;        rlf     Tens,f
- ;       rlf     Ones,f
- ;       comf    Ones,f
-  ;      rlf     Ones,f
-;
-;        movf    NumL,w
- ;      andlw   0X0F
- ;       addwf   Ones,f
- ;       rlf     Thou,f
+        movf    NumH,w
+        andlw   0X0F
+        addwf   Hund,f
+        addwf   Hund,f
+       addwf   Ones,f
+        addlw   0XE9
+        movwf   Tens
+        addwf   Tens,f
+       addwf   Tens,f
 
- ;       movlw   0X07
- ;       movwf   TenK
+        swapf   NumL,w
+        andlw   0X0F
+        addwf   Tens,f
+        addwf   Ones,f
+
+        rlf     Tens,f
+        rlf     Ones,f
+        comf    Ones,f
+        rlf     Ones,f
+
+        movf    NumL,w
+       andlw   0X0F
+        addwf   Ones,f
+        rlf     Thou,f
+
+        movlw   0X07
+        movwf   TenK
 
                     ; At this point, the original number is
                     ; equal to
@@ -580,29 +585,29 @@ pop
                     ; needs to be normalized, but this can all be
                     ; done with simple byte arithmetic.
 
- ;       movlw   0X0A                             ; Ten
-;Lb1:
-;        addwf   Ones,f
-;        decf    Tens,f
-;        btfss   3,0
- ;       goto   Lb1
-;Lb2:
-;        addwf   Tens,f
- ;;       decf    Hund,f
- ;       btfss   3,0
- ;       goto   Lb2
-;Lb3:
-;        addwf   Hund,f
-;        decf    Thou,f
- ;       btfss   3,0
-;        goto   Lb3
-;Lb4:
- ;       addwf   Thou,f
- ;       decf    TenK,f
-;        btfss   3,0
-;        goto   Lb4
+        movlw   0X0A                             ; Ten
+Lb1:
+        addwf   Ones,f
+        decf    Tens,f
+        btfss   3,0
+        goto   Lb1
+Lb2:
+        addwf   Tens,f
+        decf    Hund,f
+        btfss   3,0
+        goto   Lb2
+Lb3:
+        addwf   Hund,f
+        decf    Thou,f
+        btfss   3,0
+        goto   Lb3
+Lb4:
+        addwf   Thou,f
+        decf    TenK,f
+        btfss   3,0
+        goto   Lb4
 
-;        retlw	0x00
+        retlw	0x00
 
 ; *****************************         Begin Multiplier Routine
 ;mpy_S   clrf    H_byte
@@ -622,45 +627,45 @@ pop
 ;	retlw   0
 
 
-;Message	
+Message	
 
-;		movf	count, w		;put counter value in W
-;		call	Text			;get a character from the text table
-;		xorlw	0x00			;is it a zero?
-;		btfsc	STATUS, Z
-;		call	NextMessage
-;		call	LCD_Char
-;		incf	count, f
-;		goto	Message
+		movf	count, w		;put counter value in W
+		call	Text			;get a character from the text table
+		xorlw	0x00			;is it a zero?
+		btfsc	STATUS, Z
+		call	NextMessage
+		call	LCD_Char
+		incf	count, f
+		goto	Message
 
-;NextMessage	
-;		movlw	d'2'
-;		call	LCD_Line2W		;move to 2nd row, third column
-;		call	Convert			;convert to decimal
-;		movf	TenK,	w		;display decimal characters
-;		call	LCD_CharD		;using LCD_CharD to convert to ASCII
-;		movf	Thou,	w
-;		call	LCD_CharD
-;		movf	Hund,	w
-;		call	LCD_CharD		
-;		movf	Tens,	w
-;		call	LCD_CharD
-;		movf	Ones,	w
-;		call	LCD_CharD
-;		movlw	' '			;display a 'space'
-;		call	LCD_Char
-;		movf	NumH,	w		;and counter in hexadecimal
-;		call	LCD_HEX
-;		movf	NumL,	w
-;		call	LCD_HEX
-;		incfsz	NumL, 	f
-;		goto	Next
-;		incf	NumH,	f
-;Next	
-;	call	Delay20		;wait so you can see the digits change
+NextMessage	
+		movlw	d'2'
+		call	LCD_Line2W		;move to 2nd row, third column
+		call	Convert			;convert to decimal
+		movf	TenK,	w		;display decimal characters
+		call	LCD_CharD		;using LCD_CharD to convert to ASCII
+		movf	Thou,	w
+		call	LCD_CharD
+		movf	Hund,	w
+		call	LCD_CharD		
+		movf	Tens,	w
+		call	LCD_CharD
+		movf	Ones,	w
+		call	LCD_CharD
+		movlw	' '			;display a 'space'
+		call	LCD_Char
+		movf	NumH,	w		;and counter in hexadecimal
+		call	LCD_HEX
+		movf	NumL,	w
+		call	LCD_HEX
+		incfsz	NumL, 	f
+		goto	Next
+		incf	NumH,	f
+Next	
+	call	Delay20		;wait so you can see the digits change
 
-;	goto	NextMessage
-;return
+	goto	NextMessage
+return
 
 
  END
